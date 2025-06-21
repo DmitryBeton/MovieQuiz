3bc51babbabd576f2e4c6d62e208ae3abdd7345d
Beton <dima41768@icloud.com>
2025-06-21 14:40:43 +0300

    Проект завершен

---
+++ MovieQuiz/QuizResultsViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  QuizResultsViewModel.swift
+//  MovieQuiz
+//
+//  Created by Дмитрий Чалов on 22.06.2025.
+//
+
+import Foundation
---
+++ MovieQuiz/QuizStepViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  QuizStepViewModel.swift
+//  MovieQuiz
+//
+//  Created by Дмитрий Чалов on 22.06.2025.
+//
+
+import Foundation
