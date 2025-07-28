f56bd18b13b68274318b9fdc196032cc924b3727
Beton <dima41768@icloud.com>
2025-07-29 01:03:57 +0300

    Рефакторинг MVC -> MVP

---
+++ MovieQuiz/Presentation/MovieQuizPresenter.swift
@@ -0,0 +1,8 @@
+//
+//  MovieQuizPresenter.swift
+//  MovieQuiz
+//
+//  Created by Дмитрий Чалов on 29.07.2025.
+//
+
+import Foundation
---
+++ MovieQuizTests/MoviesLoaderTests.swift
@@ -0,0 +1,8 @@
+//
+//  MoviesLoaderTests.swift
+//  MovieQuizTests
+//
+//  Created by Дмитрий Чалов on 26.07.2025.
+//
+
+import Foundation
